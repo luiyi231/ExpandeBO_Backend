1cff2c6763982a24ceb48f850a7bba2619fa0b60
Lenovo <luiyimateoencinas@gmail.com>
2025-12-03 10:37:03 -0400

    update

---
+++ Models/DTOs/UpdateRepartidorRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace ExpandeBO_Backend.Models.DTOs;
+
+public class UpdateRepartidorRequest
+{
+    public double? Latitud { get; set; }
+    public double? Longitud { get; set; }
+    public double? ProgresoRuta { get; set; } // 0.0 a 1.0
+    public string? RutaGeometry { get; set; } // Polyline codificado
+    public int? DuracionRutaSegundos { get; set; }
+}
+
